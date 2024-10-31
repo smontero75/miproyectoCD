0eabc5d61d2d9e40ddf25da36acf2a1682632fe5
SEBASTIAN MONTERO <sebast.montero@gmail.com>
2024-10-31 11:27:48 -0300

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal, Signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ListProductsComponent } from './list-products/list-products.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ListProductsComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'miproyectoCD';
+  title = 'miproyectoCD';  
+  product = "";
+  listProducts = signal<string[]>([])  
+
+  constructor(){
+    this.listProducts.set(["Mayonesa", "Ketchup", "Mostaza"]);
+  }
+  
+  addProduct() {
+    this.listProducts.update(products => {
+      return [...products, this.product];
+    });  
+
+    //this.listProducts.set(["Mayonesa222", "Ketchup222", "Mostaza2222"]);
+    
+  }
 }
---
+++ src/app/list-products/list-products.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input, Input, OnChanges, signal, SimpleChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-list-products',
+  standalone: true,  
+  templateUrl: './list-products.component.html',
+  styleUrl: './list-products.component.css',
+  imports:[CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ListProductsComponent implements OnChanges {
+  
+  @Input() listProducts = signal<string[]>([]);  
+  
+  
+
+  ngAfterViewInit(){    
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("algo cambio");
+  }
+
+
+}
